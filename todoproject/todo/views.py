29adb15422d8091a4de29ad59213e0a2e197e0ce
Balraj <balrajkv666@gmail.com>
2025-09-09 19:55:28 +0530

    Add files via upload

---
+++ todoproject/todo/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from .models import Task
+
+# Create your views here.
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'todo/index.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        Task.objects.create(title=title)
+    return redirect('index')
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('index')
+
+def toggle_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect('index')
